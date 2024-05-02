00a539bbc3025c8e38ab3eb0dd7b1036c3bca88b
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-02 19:47:56 -0400

    fixed syntax errors in webpack that I created :(

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,27 +17,24 @@ module.exports = () => {
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
+
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE'
+        title: 'J.A.T.E.'
       }),
      
-      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
+        swDest: 'src-sw.js',
       }),
 
-      // Creates a manifest.json file.
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'JATE',
-        short_name: 'JATE',
+        name: 'J.A.T.E.',
+        short_name: 'J.A.T.E.',
         description: 'Just Another Text Editor!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
@@ -45,7 +42,7 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('./src/images/logo.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
