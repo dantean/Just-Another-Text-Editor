5d4deae684cef496e348f3b91c3e9f8f66e962e2
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-02 18:46:01 -0400

    fixed another syntax error in webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve('./src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
