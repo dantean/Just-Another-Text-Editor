cb35290e1856f858e088eaf653e0a2cf08270e5f
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-02 20:08:31 -0400

    fixed more syntax errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
@@ -23,7 +22,6 @@ export const putDb = async (content) => {
   console.log('Data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
@@ -34,3 +32,5 @@ export const getDb = async () => {
   console.log('result.value', result);
   return result;
 };
+
+initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,9 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 
-    // Store the triggered events
     window.deferredPrompt = event;
 
-    // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
   });
 
@@ -19,16 +15,14 @@ butInstall.addEventListener('click', async () => {
    return;
   }
 
-  // Show prompt
   promptEvent.prompt();
   
-  // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
   
   butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
-  // Clear prompt
+
   window.deferredPrompt = null;
 }); 
